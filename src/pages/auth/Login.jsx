d2fa6fb2ae0df67941a7c9bcde747fdb9c974f1a
Clynova <clynova.dev@gmail.com>
2025-02-10 01:47:19 -0300

    Se cambio login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,5 +1,9 @@
 import { useGlobal } from '../../context/GlobalContext';
 import { useEffect } from 'react';
+import logo from "../../images/logo.svg";
+import googleIconImageSrc from "../../images/google-icon.png";
+import twitterIconImageSrc from "../../images/twitter-icon.png";
+import illustration from "../../images/login-illustration.svg";
 
 const Login = () => {
     const { setPageTitle } = useGlobal();
@@ -9,50 +13,84 @@ const Login = () => {
         setPageTitle(`${newPageName} | LynFront`);
     }, [setPageTitle]);
 
+    const logoLinkUrl = "#";
+    const socialButtons = [
+        {
+            iconImageSrc: googleIconImageSrc,
+            text: "Sign Up With Google",
+            url: "https://google.com"
+        },
+        {
+            iconImageSrc: twitterIconImageSrc,
+            text: "Sign Up With Twitter",
+            url: "https://twitter.com"
+        }
+    ];
+    const submitButtonText = "Sign Up";
+    const forgotPasswordUrl = "#";
+    const signupUrl = "#";
+
     return (
-        <section className="bg-gray-50 dark:bg-gray-900">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
-                    Flowbite
-                </a>
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Sign in to your account
-                        </h1>
-                        <form className="space-y-4 md:space-y-6" action="#">
-                            <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
-                            </div>
-                            <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
-                                    </div>
-                                    <div className="ml-3 text-sm">
-                                        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
-                                    </div>
-                                </div>
-                                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                            </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                                Sign in
-                            </button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                            </p>
-                        </form>
+        <section className="bg-gray-50 dark:bg-gray-900 min-h-screen flex items-center justify-center p-4">
+            <div className="w-4/5 bg-white text-gray-900 shadow-md rounded-lg p-6 md:flex md:items-center"> {/* Ajuste ancho del contenedor */}
+                <div className="w-full md:w-1/2">
+                    <a href={logoLinkUrl} className="block text-center mb-8">
+                        <img src={logo} alt="Logo" className="h-12 mx-auto" />
+                    </a>
+
+                    <div className="text-center">
+                        <h1 className="text-2xl font-extrabold">Sign Up For Treact</h1>
                     </div>
+
+                    <div className="mt-6">
+                        {socialButtons.map((socialButton, index) => (
+                            <a
+                                key={index}
+                                href={socialButton.url}
+                                className="w-full flex items-center justify-center px-4 py-2 border rounded-lg bg-gray-100 hover:bg-gray-200 transition text-sm mt-4"
+                            >
+                                <img src={socialButton.iconImageSrc} className="w-5 h-5" alt="Icon" />
+                                <span className="ml-3">{socialButton.text}</span>
+                            </a>
+                        ))}
+                    </div>
+
+                    <div className="my-6 text-center text-gray-600 text-sm">Or Sign up with your e-mail</div>
+
+                    <form className="space-y-4">
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            className="w-full px-4 py-3 border rounded-lg bg-gray-100 focus:outline-none focus:border-gray-400"
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            className="w-full px-4 py-3 border rounded-lg bg-gray-100 focus:outline-none focus:border-gray-400"
+                        />
+                        <button
+                            type="submit"
+                            className="w-full py-3 bg-primary-500 text-white rounded-lg hover:bg-primary-700 transition"
+                        >
+                            {submitButtonText}
+                        </button>
+                    </form>
+
+                    <div className="mt-6 text-center text-sm">
+                        <p>I agree to abide by treact's Terms of Service and its Privacy Policy</p>
+                        <p className="mt-4">
+                            Already have an account? <a href={signupUrl} className="text-primary-500">Sign in</a>
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className="hidden md:block md:w-1/2 md:pl-8"> {/* Agregado padding a la izquierda */}
+                    <img src={illustration} alt="Illustration" className="w-3/4 h-auto" /> {/* Ajuste ancho de imagen */}
                 </div>
             </div>
         </section>
     );
 };
 
-export { Login }
+export { Login };
