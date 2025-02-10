a71f70d99a300ab0516d9d148c547dbeed5fb75a
Clynova <clynova.dev@gmail.com>
2025-02-09 23:14:09 -0300

    Se agrego contdo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,6 +17,7 @@ const Home = () => {
     <MainLayout>
       <h1>Bienvenido a nuestra tienda de accesorios para automóviles</h1>
       {/* Más contenido de la página de inicio */}
+      Aqui agregamos el contenido
     </MainLayout>
   );
 };
