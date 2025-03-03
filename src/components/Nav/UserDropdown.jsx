1570c9a4fee12ad0b4a941fcf710f6f345e5df76
Clynova <clynova.dev@gmail.com>
2025-03-03 00:20:23 -0300

    Se arregla rediccion a carrito

--- src/components/Nav/UserDropdown.jsx
+++ src/components/Nav/UserDropdown.jsx
@@ -26,7 +26,7 @@ const UserDropdown = () => {
   
   const menuItems = [
     { icon: <HiUser className="w-5 h-5" />, label: 'Mi Perfil', to: '/profile' },
-    { icon: <HiShoppingCart className="w-5 h-5" />, label: 'Carrito', onClick: () => setIsCartOpen(true) },
+    { icon: <HiShoppingCart className="w-5 h-5" />, label: 'Carrito', to: '/checkout' },
     { icon: <HiShoppingBag className="w-5 h-5" />, label: 'Mis Compras', to: '/profile/orders' },
     { icon: <HiCog className="w-5 h-5" />, label: 'Configuración', to: '/profile/settings' },
     { icon: <HiLogout className="w-5 h-5" />, label: 'Cerrar Sesión', onClick: handleLogout },
