4745f1e823c5cd19136b79ca81ab8dda37fb7eac
Clynova <clynova.dev@gmail.com>
2025-03-07 14:15:59 -0300

    test

--- src/utils/funcionesReutilizables.js
+++ src/utils/funcionesReutilizables.js
@@ -24,8 +24,8 @@ const formatCurrency = (amount) => {
     return new Intl.NumberFormat('es-CL', {
         style: 'currency',
         currency: 'CLP',
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 0
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
     }).format(amount);
 };
 
